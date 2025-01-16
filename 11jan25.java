7d56d6fb0747f5e2582d2fecfa4e6befb4ebd323
Krishna Sharma <147537973+Krishn-10@users.noreply.github.com>
2025-01-16 15:06:26 +0530

    Create 11jan25.java

---
+++ 11jan25.java
@@ -0,0 +1,25 @@
+// Longest substring with distinct characters
+// 11-01-2025
+//Space Comp - O(1)
+//Time Comp - O(n*2)
+
+class Solution {
+    public int longestUniqueSubstr(String s) {
+        int ans = 1;
+        int i=0;
+        int j=0;
+        while(i<s.length()){
+            int currmax = 1;
+            if(j==s.length())
+                return ans;
+            while(j<s.length()-1 && !((s.substring(i,j+1)).contains((String.valueOf(s.charAt(j+1)))))){
+                j++;
+                currmax++;
+            }
+            i++;
+            j=i;
+            ans = Math.max(currmax,ans);
+        }
+        return ans;
+    }
+}
